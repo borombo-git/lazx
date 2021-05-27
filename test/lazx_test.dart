748e5fb916b293bf8fc4484952c181af5993f225
Borombo <e.rombo@yahoo.com>
2021-05-27 11:29:58 +0200

    [TESTS] : Add test on LazxData state

--- test/lazx_test.dart
+++ test/lazx_test.dart
@@ -4,15 +4,26 @@ import 'package:lazx/lazx.dart';
 void main() {
   group('LazxData Tests', () {
     group('Simple Lazx Data', () {
-      test('check the right initial value', () {
+      test('Test the right initial value', () {
         final data = LazxData<int>(1);
         expect(data.value, 1);
       });
 
-      test('check the right value after update', () {
+      test('Test the right value after update', () {
         final data = LazxData<int>(1);
         data.push(2);
         expect(data.value, 2);
+      });
+
+      test('Test the right initial state', () {
+        final data = LazxData<int>(1);
+        data.state.listen(
+          expectAsync1(
+            (event) {
+              expect(event, LxState.Initial);
+            },
+          ),
+        );
       });
     });
   });
