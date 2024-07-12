43fd78a8c057c6e0b1a7926c0dcf9110e70a946e
Borombo <e.rombo@yahoo.com>
2024-07-13 01:17:04 +0200

    [FIX]: Fix build

--- demos/lazx-ideas/lib/manager/UserManager.dart
+++ demos/lazx-ideas/lib/manager/UserManager.dart
@@ -1,10 +1,6 @@
-import 'package:lazx_idea/ui/auth/auth_repository.dart';
-
 class UserManager {
   static UserManager? _instance;
   factory UserManager() => _instance ??= UserManager._();
-
-  final AuthRepository _authRepository = AuthRepository();
 
   UserManager._();
 }
